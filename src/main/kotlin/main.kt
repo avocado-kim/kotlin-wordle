2b50ed2616f30ee2c480e3a5fccfbbd65da833b7
tongnamuu <thban33@naver.com>
2026-01-11 15:10:53 +0900

    게임 로직 초록색 완성 노란색 작업중 - 맥뎁

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -11,18 +11,39 @@ fun main(args: Array<String>) {
     val daySinceStart = ChronoUnit.DAYS.between(startDate, LocalDate.now())
     val index = (daySinceStart % words.size).toInt()
     val answer = words[index]
+
     println(answer)
     val maxCount = 6
     var currentCount = 0
     while (currentCount < maxCount) {
-        val line = readLine()!!.trim()
-        if (line.length != 5) {
+        val inputWord = readLine()!!.trim()
+        if (inputWord.length != 5) {
             println("5글자를 입력해주세요")
             continue
         }
-        if (line == answer) {
+
+        val inputWordChars = inputWord.toMutableList()
+        val answerChars = answer.toMutableList()
+        val emojiArray = MutableList(inputWordChars.size) { Color.GREY.colorBox }
+        for (i in inputWordChars.indices) {
+            if (inputWordChars[i] == answerChars[i]) {
+                emojiArray[i] = Color.GREEN.colorBox
+            } else {
+                emojiArray[i] = Color.GREY.colorBox
+            }
+        }
+
+        for (i in emojiArray.indices) {
+            if (emojiArray[i] == Color.GREY.colorBox
+                && answer.contains(inputWordChars[i])) {
+                emojiArray[i] = Color.YELLOW.colorBox
+            }
+        }
+
+        println(emojiArray)
+
+        if (inputWord == answer) {
             println("good 시도횟수: ${currentCount}")
-            println(Color.GREEN.colorBox.repeat(5))
         } else {
             currentCount++
             println("bad 시도횟수: ${currentCount}")
