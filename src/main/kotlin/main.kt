fe68a3f30ead60b01a9936f6a596bbd9db836e55
tongnamuu <thban33@naver.com>
2026-01-11 14:38:54 +0900

    5글자 검증을 포함한 입력처리

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,7 @@
 import java.io.FileInputStream
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
+import java.util.*
 
 fun main(args: Array<String>) {
     val words = FileInputStream("/Users/taehyeongban/Desktop/Project/kotlin-wordle/src/main/resources/words.txt")
@@ -10,5 +11,22 @@ fun main(args: Array<String>) {
     val daySinceStart = ChronoUnit.DAYS.between(startDate, LocalDate.now())
     val index = (daySinceStart % words.size).toInt()
     val answer = words[index]
+    println(answer)
+    val maxCount = 6
+    var currentCount = 0
+    while (currentCount < maxCount) {
+        val line = readLine()!!.trim()
+        if (line.length != 5) {
+            println("5글자를 입력해주세요")
+            continue
+        }
+        if (line == answer) {
+            println("good 시도횟수: ${currentCount}")
+        } else {
+            currentCount++
+            println("bad 시도횟수: ${currentCount}")
+        }
+    }
+
 
 }
