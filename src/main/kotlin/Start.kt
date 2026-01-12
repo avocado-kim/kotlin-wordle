cd0c3f6362559f6ce482c7cd1322afa03bfd3964
tongnamuu <thban33@naver.com>
2026-01-12 22:34:22 +0900

    words.txt 상대경로 변경

--- src/main/kotlin/Start.kt
+++ src/main/kotlin/Start.kt
@@ -5,13 +5,10 @@ import java.time.temporal.ChronoUnit
 class Start {
 
     fun findAnswer() :GameString {
-        val words = FileInputStream("/Users/taehyeongban/Desktop/Project/kotlin-wordle/src/main/resources/words.txt")
-            .bufferedReader()
-            .readLines()
+        val words = this::class.java.getResourceAsStream("words.txt").bufferedReader().readLines();
         val startDate = LocalDate.of(2021,6,19)
         val daySinceStart = ChronoUnit.DAYS.between(startDate, LocalDate.now())
         val index = (daySinceStart % words.size).toInt()
-
         return GameString(words[index])
     }
 }
