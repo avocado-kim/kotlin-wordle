d156b3ba3c2f3109f04e4bd55959e605985bfa30
apocalypse7050@gmail.com <pota@codeindesign.kr>
2026-01-19 23:53:21 +0900

    인트로 안내 메세지 추가

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,8 +1,14 @@
 package view
 
+import domain.Constant
 import domain.GameString
 
 class InputView {
+    fun printIntro() {
+        println("WORDLE을 ${Constant.MAX_GAME_TRY_COUNT}번 만에 맞춰 보세요.")
+        println("시도의 결과는 타일의 색 변화로 나타납니다.")
+    }
+
     fun printInputPrompt() {
         println("정답을 입력해 주세요.")
     }
